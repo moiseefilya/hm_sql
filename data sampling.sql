785f2086b07d3d846de1990bf1abddfddeeb904d
moiseefilya <Ilyamoiseev02@yandex.ru>
2022-11-22 00:31:21 +0300

    new commit

--- data sampling.sql
+++ data sampling.sql
@@ -8,9 +8,10 @@ order by count_artist desc;
 
 --Запрос 2
 
-select count(name)
+select count(*) as count_track
 from track
-where album_id = (select id from albums where data_publicate >= '2019-01-01' and data_publicate <= '2020-12-31');
+join albums on album_id = albums.id 
+where albums.data_publicate between '2019-01-01' and '2020-12-31'
 
 --Запрос 3
 
@@ -22,11 +23,13 @@ order by avg_dur desc;
 
 --Запрос 4
 
-select name
-from artists as art  
-join albums as alb on alb.id = art.id 
-where alb.data_publicate < '2020-01-01' or alb.data_publicate > '2021-01-01'
-group by name, art.id;
+select artists.name
+from artists 
+where artists.name not in ( select artists.name 
+							from artists
+							join artistalbums on artists.id = artistalbums.artist_id 
+							join albums on artistalbums.albums_id = albums.id 
+							where albums.data_publicate between '2020-01-01' and '2020-12-31' );
 
 --Запрос 5 
 
@@ -41,60 +44,47 @@ where art.name like 'ATL';
 
 --Запрос 6
 
-select art_alb.album_name
-from (
-	select artists.id as id, artists.name as artist_name, count(*) as count_styles
-	from styleartist
-	join styles on styles_id = styles.id
-	join artists on artists_id = artists.id
-	group by artists.id, artists.name
-) as art_sty
-join (
-	select artists.id as artist_id, albums.id as album_id, albums.album_name as album_name
-	from artistalbums 
-	join albums on albums_id = albums.id
-	join artists on artist_id = artists.id
-) as art_alb on art_alb.artist_id = art_sty.id
-where art_sty.count_styles > 1
+select albums.album_name as album_name
+from albums 
+join artistalbums on albums.id = artistalbums.albums_id 
+join artists on artistalbums.artist_id = artists.id 
+join styleartist on artists.id = styleartist.artists_id 
+join styles on styleartist.styles_id = styles.id 
+group by album_name
+having count(styles.name) > 1
+
 
 --Запрос 7
 
-select track.name
-from (
-	select track.id as track_id, collection.id as col_id, track.name as track_name
-	from collectiontrack 
-	join collection on collection_id = collection.id 
-	join track on track_id = track.id 
-	group by track.id, collection.id, track.name
-) as col_track
-full outer join track on track_id = track.id 
-where track_id is null 
+select track.name as track_name
+from track
+left join collectiontrack on track.id = collectiontrack.track_id 
+left join collection on collectiontrack.collection_id = collection.id 
+where collection.name is null
+
 
 --Запрос 8
 
-select art_alb.art_name
-from (
-	select artists.id as art_id, albums.id as alb_id, artists.name as art_name
-	from artistalbums
-	join artists on artist_id = artists.id 
-	join albums on albums_id = albums.id 
-	group by artists.id, albums.id, artists.name
-) as art_alb
-join track on art_alb.alb_id = track.album_id
-where duration = ( select min(duration) from track )
+select artists.name as artist_name
+from artists
+join artistalbums on artists.id = artistalbums.artist_id 
+join albums on artistalbums.albums_id = albums.id 
+join track on albums.id = track.album_id 
+group by artists.name, track.duration 
+having track.duration = ( select min(track.duration) from track )
 
 --Запрос 9
 
-select alb_track.album_name
-from (
-	select albums.id, albums.album_name, count(track.id) as count_track
-	from track
-	join albums on track.album_id = albums.id
-	group by albums.id, albums.album_name
-	having count(track.id) = -- понимаю что нужен select запрос min(count_track), но не получается выполнить 
-	order by count_track desc
-) as alb_track
-group by alb_track.album_name, alb_track.count_track
+select albums.album_name 
+from albums
+join track on albums.id = track.album_id
+group by albums.album_name 
+having count(track.id) = ( select count(track.id) as count_track
+							from albums
+							join track on albums.id = track.album_id 
+							group by albums.album_name 
+							order by count_track asc
+							limit 1);
 
 
  
